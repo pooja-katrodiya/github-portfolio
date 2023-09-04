484b05f072f6e57350876a972b3069471412831d
Pooja Katrodiya <poojakatrodiyacanada@gmail.com>
2023-09-03 21:37:19 -0400

    Updating profile link

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     name: 'Pooja Katrodiya',
     title: "Data Scientist",
     desciption:"TODO     TODO     TODO     TODO     TODO     TODO     TODO     TODO     TODO     TODO     TODO     TODO     TODO     TODO     TODO     TODO     TODO     TODO     TODO     TODO     TODO     TODO     TODO     TODO     TODO     TODO",
-    image: 'https://raw.githubusercontent.com/pooja-katrodiya/github-portfolio/assets/Pooja%20Katrodiya.jpg',
+    image: 'https://raw.githubusercontent.com/pooja-katrodiya/github-portfolio/assets/Profile/Pooja%20Katrodiya.jpg',
     resumePdf: resume
 }
