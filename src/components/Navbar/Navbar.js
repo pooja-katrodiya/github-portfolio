b349f61427a07b7808fbed70233edf7cbf86317d
Pooja Katrodiya <poojakatrodiyacanada@gmail.com>
2023-09-06 20:25:08 -0400

    Updated navbar items

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -229,7 +229,7 @@ function Navbar() {
                             </NavLink>
                         </Fade>
 
-                        <Fade left>
+                        {/* <Fade left>
                             <NavLink
                                 to='/#services'
                                 smooth={true}
@@ -245,9 +245,9 @@ function Navbar() {
                                     </span>
                                 </div>
                             </NavLink>
-                        </Fade>
+                        </Fade> */}
 
-                        <Fade left>
+                        {/* <Fade left>
                             <NavLink
                                 to='/#blog'
                                 smooth={true}
@@ -263,7 +263,7 @@ function Navbar() {
                                     </span>
                                 </div>
                             </NavLink>
-                        </Fade>
+                        </Fade> */}
 
                         <Fade left>
                             <NavLink
