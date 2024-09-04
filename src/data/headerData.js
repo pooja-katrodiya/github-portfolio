21638f326ac9130a3729ae4ddea123218939c7ca
Pooja_Katrodiya <poojakatrodiyacanada@gmail.com>
2024-09-04 15:09:23 -0400

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -3,7 +3,7 @@ import resume from '../assets/pdf/resume.pdf'
 export const headerData = {
     name: 'Pooja Katrodiya',
     title: "AI and Data Science Student",
-    desciption:"My name is Pooja. I am a post graduate student studying Artifical Intelligence and Data science based in Ontario, Canada. I am excited to express my keen interest in seeking a data science/analyst intern position for the Summer of 2024.",
+    desciption:"My name is Pooja. I am a data enthusiast who studied Artifical Intelligence and Data science post-graduate program in Ontario, Canada. I am excited to express my keen interest in seeking a data science/analyst intern position for the Summer of 2024.",
     image: 'https://raw.githubusercontent.com/pooja-katrodiya/github-portfolio/assets/Profile/Pooja%20Katrodiya.jpg',
     resumePdf: resume
 }
